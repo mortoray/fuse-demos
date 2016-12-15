e2b9e05a9fccc6cd24460d0c69e0d3755449cbce
edA-qa on MBP <edA-qa@disemia.com>
2016-12-15 10:58:34 +0100

    migrate to here

---
+++ pixabay/pixabay.js
@@ -0,0 +1,5 @@
+// Put your pixabay key as a string here
+exports.key = "YOUR PIXABAY KEY FROM https://pixabay.com/api/docs/"
+exports.key = "401922-2c93ef5c961e023d90594333e"
+
+exports.search = "https://pixabay.com/api/"
