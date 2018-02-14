0d6bf34616f9cac5da94c15d742537c82966f668
edA-qa on MBP <edA-qa@disemia.com>
2018-02-14 16:49:14 +0100

    end  of part2 stream

--- signup-transition/MainState.js
+++ signup-transition/MainState.js
@@ -1,5 +1,6 @@
 class PageLogin {
-	constructor() {
+	constructor(mainState) {
+		this.mainState = mainState
 		this.email = "login@smply.com"
 		this.password = "1231312321"
 	}
@@ -10,10 +11,16 @@ class PageLogin {
 	
 	forgotPassword() {
 	}	
+	
+	action() {
+		this.mainState.gotoLoading()
+		setTimeout( () => { this.mainState.gotoHome() }, 1000 )
+	}
 }
 
 class PageRegister {
-	constructor() {
+	constructor(mainState) {
+		this.mainState = mainState
 		this.email = "sample@smply.com"
 		this.password = "1231312321"
 		this.password2 = ""
@@ -23,12 +30,78 @@ class PageRegister {
 		return this.email != "" && this.password != "" &&
 			this.password == this.password2
 	}
+	
+	action() {
+		//TODO: implement
+	}
+}
+
+class PageLogout {
+	constructor(mainState) {
+		this.$path = 'logout'
+		this.mainState = mainState
+		this.loginPages = [  new PageLogin(mainState), new PageRegister(mainState) ]
+	}
+}
+
+class PageLoading {
+	constructor(mainState) {
+		this.$path = 'loading'
+	}
+}
+
+class PageHome {
+	constructor(mainState) {
+		this.mainState = mainState
+		this.$path = 'home'
+		this.$navigationRequest = {
+			transition: 'Transition',
+			style: 'Push',
+		}
+	}
 }
 
 export default class MainState {
 	constructor() {
 		this.title ="Login Transitions"
 		
-		this.loginPages = [  new PageLogin(), new PageRegister() ]
+		this.mainPages = [ new PageLogout(this) ] 
+		//this.mainPages = [ new PageLoading(this) ]
+	}
+	
+	gotoLoading() {
+		//TODO: replace with Goto
+		//this.mainPages[0] = new PageLoading(this)
+		this.mainPages.push( new PageLoading(this) )
+	}
+	
+	gotoHome() {
+		//TODO: something weird here
+		//this.mainPages.pop();
+		//this.mainPages[0] = new PageHome(this)
+		//this.mainPages = [ new PageHome(this) ]
+		this.mainPages.push( new PageHome(this) );
+	}
+	
+	logout() {
+		this.mainPages = [ new PageLogout(this) ]
 	}
 }
+
+/**
+File name: FuseJS/Internal/zone.js
+Line number: 196
+Script stack trace: Error: Internal error: Attempted to detach child that is not attached to us
+    at removeAsParentFrom (FuseJS/Internal/Model.js:320:12)
+    at Object.meta.diff (FuseJS/Internal/Model.js:250:7)
+    at update (FuseJS/Internal/Model.js:392:15)
+    at Object.meta.diff (FuseJS/Internal/Model.js:266:6)
+    at update (FuseJS/Internal/Model.js:392:15)
+    at Object.meta.diff (FuseJS/Internal/Model.js:266:6)
+    at FuseJS/Internal/Model.js:212:11
+    at ZoneDelegate.invokeTask (FuseJS/Internal/zone.js:425:31)
+    at Zone.runTask (FuseJS/Internal/zone.js:192:47)
+    at ZoneTask.invokeTask (FuseJS/Internal/zone.js:499:34)
+<---
+
+*/
