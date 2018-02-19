b37d9fac946cb2983401864250bad685d2870c54
edA-qa on MBP <edA-qa@disemia.com>
2018-02-19 15:53:23 +0100

    streamed example of a simple bank transfer patch panel

---
+++ stream/patch-line/MainState.js
@@ -0,0 +1,6 @@
+export default class MainState {
+	constructor() {
+		this.accounts = [ "Savings", "Chequeing", "Credit Card" ]
+		this.recipients = [ "Brother", "Parents", "Steam", "Shady Guy" ]
+	}
+}
